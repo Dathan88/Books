13187f249346bb5e5ad5d57909204418b5b4c467
Dathan Spencer <dpspencer88@gmail.com>
2018-07-19 17:59:49 -0600

    Complete project skeleton

--- Books.js
+++ Books.js
@@ -1,15 +1,19 @@
 const books = document.getElementById('myBooks');
 const theHobbit =  new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false);
-
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
 	this.title = title,
 	this.author = author,
 	this.pages = pages,
-	this.read = true,
+	this.read = (read === true) ? "Yes" : "Not read yet",
 	this.info = function() {
 		return this.title + " by " + this.author + " , " + this.pages + " , " + this.read;
 	};
+}
+
+function addBookToLibrary () {
+
 }
 
 console.log(theHobbit.info());
