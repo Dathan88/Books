350224dc1ec16c656f9b065a7d27b406e406a7a2
Dathan Spencer <dpspencer88@gmail.com>
2018-07-31 18:46:24 -0600

    Trying to create table rows with dom

--- Books.js
+++ Books.js
@@ -1,7 +1,7 @@
 const bookDisplay = document.getElementById('myBooks');
 const myForm = document.getElementById("myForm");
 const hobbit =  new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 0);
-let myLibrary = [hobbit];
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
 	this.title = title,
@@ -24,34 +24,37 @@ function addBookToLibrary() {
 	let author = document.getElementById('author').value;
 	let pages = document.getElementById('pages').value;
 	let read = document.querySelector('input[name="read"]:checked').value;
-
 	const book1 = new Book(title, author, pages, read);
 
 	console.log(book1);
 	myLibrary.push(book1);
+	document.getElementById("myTr").remove();
+	render();
 	console.log(myLibrary);
 }
 
 function render(b) {
-	let table = document.getElementById("myTable");
 
-    myLibrary.forEach((item, index) => {
+    myLibrary.forEach((book) => {
+		let table = document.getElementById("myTable");
+		let row = document.createElement("TR");
+			row.setAttribute("id", "myTr");
+		table.appendChild(row);
+
     	for(let key in item) {
     		if(typeof item[key] === "function") {
     			break;
-    		} 
-    		let row = document.createElement("TR");
-    			row.setAttribute("id", "myTr");
-				document.getElementById("myTable").appendChild(row);
+    		}
 
-			let bookData = document.createElement("TD");
-			let text = document.createTextNode(item[key]);
-				bookData.appendChild(text);
-			document.getElementById("myTr").appendChild(bookData);
-  		};
+		let bookData = document.createElement("TD");
+		let text = document.createTextNode(item[key]);
+			bookData.appendChild(text);
+		document.getElementById("myTr").appendChild(bookData);
+
+  		}
     });
 }
 
-
 console.log(hobbit.info());
-console.log();
+
+document.getElementsByTagName("body").onload = myLibrary.push(hobbit), render();
