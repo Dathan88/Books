051ab0c5185e140cb2a5aa24ec18ad1d8c5b7e12
Dathan Spencer <dpspencer88@gmail.com>
2018-08-02 18:18:53 -0600

    Refine table layout

--- Books.js
+++ Books.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 const bookDisplay = document.getElementById('myBooks');
 const myForm = document.getElementById("myForm");
 const hobbit =  new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 0);
@@ -7,15 +8,14 @@ function Book(title, author, pages, read) {
 	this.title = title,
 	this.author = author,
 	this.pages = pages,
-	this.read = (read >= 1) ? "Yes" : "No",
+	this.read = (read >= 1) ? "Have Read" : "Not Read Yet  ",
 	this.info = function() {
-		return this.title + " by " + this.author + " , " + this.pages + " , " + this.read;
+		return  this.title + " by " + this.author + " - " + this.pages + " - " + this.read;
 	};
 }
 
 function newBook() {
 	document.getElementById("formContainer").style.display = "block";
-
 	return false;
 }
 
@@ -25,38 +25,44 @@ function addBookToLibrary() {
 	let pages = document.getElementById('pages').value;
 	let read = document.querySelector('input[name="read"]:checked').value;
 	const book1 = new Book(title, author, pages, read);
-
-	myLibrary.push(book1);
-
+		myLibrary.push(book1);
 	let tbody = document.getElementById("tableBody");
-	tbody.innerHTML = '';
-
+		tbody.innerHTML = '';
 	render();
 }
 
 function render() {
-	console.log(myLibrary);
 	let table = document.getElementById("myTable");
 	let tbody = document.getElementById("tableBody");
 
 	myLibrary.forEach((item, index) => {
-		let i = 0;
 		let rowPlace = index;
 		let row = tbody.insertRow(rowPlace);
-		row.setAttribute('id', 'myRows');
-			
-		for(let key in item) {
-			if(typeof item[key] === "function") {
-				continue;
-			}
-			let bookData = row.insertCell(i);
-			bookData.innerHTML = item[key];
+			row.setAttribute('id', item.title);
+		let bookData = row.insertCell(0);
+			bookData.innerHTML = item.info();
+		let btn = document.createElement("BUTTON");
+		let text = document.createTextNode("Delete " + item.title);
+			btn.appendChild(text);
+			row.insertAdjacentElement("beforeend", btn);
+		btn.addEventListener("click", (e) => {
 
-			i++;
-		};
+		});
     });
 }
 
-
 console.log(hobbit.info());
 document.getElementsByTagName("body").onload = myLibrary.push(hobbit), render();
+
+
+// for(let key in item) {
+// 			if(typeof item[key] === "function") {
+// 			let text = document.createTextNode("Delete " + item.title);
+// 			btn.appendChild(text);
+// 				continue;
+// 			}
+
+// 		let bookData = row.insertCell(i);
+// 			bookData.innerHTML = item[key];
+// 			bookData.insertAdjacentElement("afterend", btn);
+// 			i++;
